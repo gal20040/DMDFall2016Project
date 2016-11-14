61b384b975ed7856fc38b799039e6fa664f0dce8
Artem Grodetskiy <gal20040@gmail.com>
2016-11-14 22:13:07 +0300

    заливаю работающую версию проекта с кнопками, полями и БД

---
+++ src/main/webapp/resources/script/bookUtil.js
@@ -0,0 +1,9 @@
+function _BookUtil(){
+    this.deleteBook = function(id) {
+        if (confirm("Are you sure?")) {
+            window.location = "/deleteBook/" + id;
+        }
+    }
+}
+
+var BookUtil = new _BookUtil();
