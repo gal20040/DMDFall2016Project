967d9c51744dfd8fb63e14c93a79d7cbff52fffb
Artem Grodetskiy <gal20040@gmail.com>
2016-11-16 01:52:06 +0300

    убрал мусор, связанный с параметрами адресной строки

--- src/main/java/ru/hotelinno/controller/BookController.java
+++ src/main/java/ru/hotelinno/controller/BookController.java
@@ -14,7 +14,6 @@ import ru.hotelinno.domain.WishfulRoomData;
 import ru.hotelinno.repository.BookRepository;
 import ru.hotelinno.validation.BookValidator;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -50,23 +49,6 @@ public class BookController {
         return "search"; //addBook
     }
 
-    /**
-     * Можно обрабатывать параметры из адресной строки.
-     * UPDATE: Решили не использовать параметры из адресной строки.
-     * @param paramString
-     * @param model
-     * @return
-     */
-//    @RequestMapping(value = "search?{paramString}", method = RequestMethod.GET) //addBook
-//    public String search(@PathVariable String paramString,
-//                          Model model){ //addBook
-//        Book book = new Book();
-//        book.setName(paramString);
-//        book.setGenre("book.setGenre");
-//        model.addAttribute("defaultSearchData", book); //bookq
-//        return "search"; //addBook
-//    }
-
     @RequestMapping(value = "search", method = RequestMethod.POST) //addBook
     public String search(@ModelAttribute("defaultSearchData") Book book,
                           BindingResult bindingResult,
@@ -78,18 +60,6 @@ public class BookController {
 
 //        this.bookRepository
 //                .search(wishfulRoomData); //addBook
-//        String returnString, cityName = "", cityGenre = "";
-//        if (!book.getName().equals("")
-//                || !book.getGenre().equals("")) {
-//            if (!book.getName().equals("")) {
-//                cityName = "cityName=" + book.getName() + "q"; TO DO String.format
-//            }
-//            if (!book.getGenre().equals("")) {
-//                cityGenre = "cityGenre=" + book.getGenre() + "a"; TO DO cityGenre TO DO String.format
-//            }
-//            returnString = String.format("search?%s&%s", cityName, cityGenre); //addBook
-//        } else
-//            returnString = "search"; //addBook
 
         WishfulRoomData wishfulRoomData = new WishfulRoomData(book.getName() + "q", book.getGenre() + "a",
                 "" + "zxc", "" + "poi");
@@ -98,86 +68,6 @@ public class BookController {
 //        return returnString;
         return "search"; //addBook
     }
-
-//    @RequestMapping(value = "search/?{paramString}", method = RequestMethod.GET) //addBook
-//    public String deleteBook(@PathVariable String paramString){
-//        WishfulRoomData wishfulRoomData = parseParamString(paramString);
-////        this.bookRepository
-////                .removeBook(id);
-//        return "redirect:/";
-//    }
-
-//    private WishfulRoomData parseParamString(String paramString) {
-////        fromName=Казань&toName=Ижевск&fromId=c43&toId=c44&when=завтра
-//        WishfulRoomData wishfulRoomData = new WishfulRoomData();
-//        ArrayList<String> listOfAllParams = wishfulRoomData.getListOfAllParams();
-//        int cityNamePosition, roomTypePosition, checkInDatePosition, checkOutDatePosition, equalSignPosition;
-//        String tempParamString, paramName;
-//        String equalSign = "=";
-//        int tempParamStringNumber;
-//        boolean paramHasBeenFounded = false;
-//
-////        int index = paramString.indexOf("?");
-////        paramString = paramString.substring(index + 1, paramString.length());
-//
-////        while (index < paramString.length()) {
-//        ArrayList<String> stringArrayList = parseParamStringWithDelimiter(paramString);
-//        while (stringArrayList.size() > 0) {
-//            tempParamStringNumber = stringArrayList.size() - 1;
-//            tempParamString = stringArrayList.get(tempParamStringNumber);
-//            equalSignPosition = tempParamString.indexOf(equalSign);
-//            assert (equalSignPosition >= 0) : "Знак '=' не найден в запросе."; //TODO сделать корректную передачу на страницу неполных данных с уведомлением.
-//            assert (equalSignPosition == 0) : "До знака '=' в запросе нет имени параметра."; //TODO сделать корректную передачу на страницу неполных данных с уведомлением.
-//            if (equalSignPosition > 0) {
-//                paramName = tempParamString.substring(0, equalSignPosition);
-//                for (int i = 0; i < listOfAllParams.size(); i++) {
-//                    if (paramName.equals(listOfAllParams.get(i))) {
-//                        paramHasBeenFounded = true;
-//                        wishfulRoomData.se
-//                        break;
-//                    }
-//                }
-//                assert (paramHasBeenFounded) : "Неизвестный параметр в запросе."; //TODO сделать корректную передачу на страницу неполных данных с уведомлением.
-//            }
-//        }
-//
-////        cityNamePosition = paramString.indexOf("city=");
-////        tempParamString = paramString.substring(cityNamePosition + 5, paramString.length());
-////        ampersandPosition = tempParamString.indexOf("&");
-//
-//
-////        }
-//
-//        return wishfulRoomData;
-//    }
-
-//    private ArrayList<String> parseParamStringWithDelimiter(String paramString) {
-//        String defaultDelimiter = "&";
-//        return parseParamStringWithDelimiter(paramString, defaultDelimiter);
-//    }
-
-    /**
-     * Parse paramString in pieces with ampersand as delimiter
-     * and return ArrayList of strings.
-     * E.g. paramString: cityName=Kazan&roomType=Standard&checkInDate=20.11.2016&checkOutDate=22.11.2016
-     * E.g. return: cityName=Kazan; roomType=Standard; checkInDate=20.11.2016; checkOutDate=22.11.2016
-//     */
-//    private ArrayList<String> parseParamStringWithDelimiter(String paramString, String delimiter) {
-//        ArrayList<String> stringArrayList = new ArrayList<String>();
-//        int ampersandPosition, equalSignPosition;
-//        String tempString, equalSign = "=";
-//        //TODO сделать проверки "амперсанд стоит первым в строке"+"амперсанда нет в строке" и обработать их.
-//        while (paramString.length() > 0) {
-//            ampersandPosition = paramString.indexOf(delimiter);
-//            tempString = paramString.substring(0, ampersandPosition);
-////            assert (tempString.indexOf(equalSign) > 0) : ""; //TODO сделать корректную передачу на страницу неполных данных с уведомлением.
-//            equalSignPosition = tempString.indexOf(equalSign);
-//            assert (equalSignPosition >= 0) : "Знак '=' не найден в запросе."; //TODO сделать корректную передачу на страницу неполных данных с уведомлением.
-//            assert (equalSignPosition == 0) : "До знака '=' в запросе нет имени параметра."; //TODO сделать корректную передачу на страницу неполных данных с уведомлением.
-//            stringArrayList.add(tempString);
-//        }
-//        return stringArrayList;
-//    }
 
     @RequestMapping(value = "deleteBook/{id}", method = RequestMethod.GET)
     @PreAuthorize("hasRole('admin')")
--- src/main/java/ru/hotelinno/domain/WishfulRoomData.java
+++ src/main/java/ru/hotelinno/domain/WishfulRoomData.java
@@ -1,25 +1,11 @@
 package ru.hotelinno.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Table;
-import java.util.ArrayList;
-
-//@Table(name = "wishfulRoomData")
 public class WishfulRoomData {
 
-//    @Column(name = "cityName")
     private String cityName;
-
-//    @Column(name = "roomType")
     private String roomType;
-
-//    @Column(name = "checkInDate")
     private String checkInDate;
-
-//    @Column(name = "checkOutDate")
     private String checkOutDate;
-
-//    public WishfulRoomData() {}
 
     public WishfulRoomData(String cityName, String roomType, String checkInDate, String checkOutDate) {
         this.cityName = cityName;
@@ -37,13 +23,4 @@ public class WishfulRoomData {
     public void setRoomType(String roomType) {this.roomType = roomType;}
     public void setCheckInDate(String checkInDate) {this.checkInDate = checkInDate;}
     public void setCheckOutDate(String checkOutDate) {this.checkOutDate = checkOutDate;}
-
-    //    public ArrayList<String> getListOfAllParams() {
-//        ArrayList<String> stringArrayList = new ArrayList<String>();
-//        stringArrayList.add(cityName.getClass().getSimpleName());
-//        stringArrayList.add(roomType.getClass().getSimpleName());
-//        stringArrayList.add(checkInDate.getClass().getSimpleName());
-//        stringArrayList.add(checkOutDate.getClass().getSimpleName());
-//        return stringArrayList;
-//    }
 }
